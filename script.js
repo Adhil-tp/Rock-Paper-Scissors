9f0a6d4f12619a1c60fc36c1aefe4be8e5674076
Adhil <mohamedadhiltp1944@gmail.com>
2023-10-31 05:30:56 -0700

    created a page and tested the logic of rock paper scissor and

---
+++ script.js
@@ -0,0 +1,26 @@
+function getComputerChoice(){
+    let list = ['ROCK','PAPER','SCISSOR'];
+    let chosen =Math.floor(Math.random() * list.length);
+    return list[chosen];
+}
+const computerValue = getComputerChoice();
+const humanselected = "ROCK" 
+
+function playRound(computerSelected , playerSelected){
+    let result ='';
+    if (computerSelected === playerSelected){
+        console.log("its a draw");
+    }else if( (computerSelected === 'ROCK' && playerSelected === 'SCISSOR') ||
+              (computerSelected === 'PAPER' && playerSelected === 'ROCK')||
+              (computerSelected === 'SCISSOR' && playerSelected === 'PAPER')){
+                result = "Computer Won ";
+              }  
+    else if((playerSelected === 'ROCK' && computerSelected === 'SCISSOR') ||
+            (playerSelected === 'PAPER' &&  computerSelected === 'ROCK') ||
+            (playerSelected === 'SCISSOR' && computerSelected === 'PAPER')){
+                result = "Human Won ";
+            }
+    return result;
+}
+
+console.log(playRound(computerValue,humanselected));
